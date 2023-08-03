529cef1bfe6dc5c6a9f618b78bd01a074aab3475
YuSoeun <doplinblue@handong.ac.kr>
2023-08-03 19:41:25 +0900

    hw3 IO multiplexin 없는 버전

---
+++ hw3/client/client.c
@@ -0,0 +1,224 @@
+/*
+	• 클라이언트가 서버에 접속하여 서버의 디렉토리와 파일정보를 확인할 수 있는 프로그램을 작성하세요.
+	• 클라이언트가 서버에 접속하면, 서버는 서버 프로그램이 실행되고 있는 디렉토리 위치 및 파일 정보(파일명 + 파일 크기)를 클라이언트에게 전달합니다.
+	• 클라이언트는 서버로부터 수신한 정보들을 출력합니다.
+	• 클라이언트는 서버의 디렉토리를 변경해가며 디렉토리 정보와 디렉토리 안에있는 파일 정보(파일명 + 파일크기)를 확인할 수 있습니다.
+	• 클라이언트가 원하는 파일을 서버로부터 다운로드 받을 수 있어야 합니다.
+	• 클라이언트는 자신이 소유한 파일을 서버에게 업로드할 수 있어야 합니다.
+	• 클라이언트가 서버에 접속하면 클라이언트는 서버 프로그램의 권한과 동일한 권한을 갖습니다. 즉, 서버 프로그램에게 허용되지 않은 디렉토리와 파일은 클라이언트도 접근할 수 없습니다.
+	• 서버는 여러 클라이언트의 요청을 동시에 지원할 수 있어야 합니다.
+		→I/O Multiplexing 사용!
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include "file_info.h"
+#include "trim.h"
+
+#define BUF_SIZE 1024
+#define FLIST_SIZE 30
+int open_socket(char *argv[]);
+void error_handling(char *message);
+int read_file_list(int sock, FileInfoPacket **files);
+int read_and_save_file(int sock, char* filename);
+void print_file_list(FileInfoPacket **files, int file_cnt);
+void print_directories(FileInfoPacket **files, int file_cnt);
+void print_files(FileInfoPacket **files, int file_cnt);
+
+int main(int argc, char *argv[])
+{
+	int sock, file_cnt, num, file_num;
+	char message[BUF_SIZE];
+	char filename[NAME_LEN];
+	char dirname[NAME_LEN];
+	int str_len, recv_len, recv_cnt;
+
+	FileInfoPacket** files;
+	files = (FileInfoPacket**)malloc((int)sizeof(FileInfoPacket*) * FLIST_SIZE);
+	for (int i = 0; i < FLIST_SIZE; i++) {
+		files[i] = (FileInfoPacket*)malloc((int)sizeof(FileInfoPacket));
+	}
+
+	if (argc != 3) {
+		printf("Usage : %s <IP> <port>\n", argv[0]);
+		exit(1);
+	}
+	
+	sock = open_socket(argv);
+	
+	// 디렉토리의 모든 파일 목록 받기
+	fputs("서버 디렉토리의 파일 목록 확인하기 (yes to continue): ", stdout);
+	fgets(message, BUF_SIZE, stdin);
+	write(sock, message, strlen(message));
+	
+	while (1) {
+		recv_cnt = read(sock, dirname, NAME_LEN);
+		while (recv_cnt < NAME_LEN) {
+			recv_cnt += read(sock, &dirname[recv_cnt], NAME_LEN - recv_cnt);
+		}
+		
+		file_cnt = read_file_list(sock, files);
+		print_file_list(files, file_cnt);
+
+		fputs("\n----- Menu ----\n", stdout);
+		fputs("1. 디렉토리 이동\n", stdout);
+		fputs("2. 파일 가져오기\n", stdout);
+		fputs("3. 파일 업로드\n", stdout);
+		fputs("4. 프로그램 종료\n", stdout);
+		fputs("1 ~ 4 중 할 번호를 선택하세요: ", stdout);
+		scanf("%d", &num);
+
+		if (num == 1) {					// 1. 디렉토리 이동
+			printf("\ncurrent directory: %s\n", dirname);
+			print_directories(files, file_cnt);
+			fputs("\n이동할 디렉토리 번호를 입력해주세요: ", stdout);
+			scanf("%d", &file_num);
+			printf("%d을 선택하였습니다.\n", file_num);
+			write(sock, files[file_num], sizeof(FileInfoPacket));
+		} else if (num == 2) {			// 2. 파일 가져오기
+			print_files(files, file_cnt);
+			fputs("\n받을 파일의 번호을 적어주세요: ", stdout);
+			scanf("%d", &file_num);
+			printf("%d을 선택하였습니다.\n", file_num);
+			write(sock, files[file_num], sizeof(FileInfoPacket));
+
+			read_and_save_file(sock, files[file_num]);
+			printf("%s is saved.\n", filename);
+		} else if (num == 3) {			// 3. 파일 업로드
+			fputs("\n업로드 할 파일 이름을 적어주세요: ", stdout);
+			scanf("%s", filename);
+			printf("%s을 업로드 하겠습니다.\n", filename);
+			strcpy(files[file_num]->name, filename);
+			strcpy(files[file_num]->type, "upload");
+			write(sock, files[file_num], sizeof(FileInfoPacket));
+		} else if (num == 4) {
+			break;
+		}
+
+	}
+
+	// // 서버에서 받아온 파일 저장
+	
+	
+	close(sock);
+	return 0;
+}
+
+/* open socket and connect server */
+int open_socket(char *argv[])
+{
+	int sock;
+	struct sockaddr_in serv_adr;
+
+	sock = socket(PF_INET, SOCK_STREAM, 0);   
+	if (sock == -1)
+		error_handling("socket() error");
+	
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family=AF_INET;
+	serv_adr.sin_addr.s_addr=inet_addr(argv[1]);
+	serv_adr.sin_port=htons(atoi(argv[2]));
+	
+	if (connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
+		error_handling("connect() error!");
+	else
+		puts("Connected...........");
+	
+	return sock;
+}
+
+/* read file list from server */
+int read_file_list(int sock, FileInfoPacket **files)
+{
+	int i = 0;
+	int recv_cnt, recv_len = 0, buffer;
+	char temp[BUF_SIZE] = {0};
+	
+	while ((recv_cnt = read(sock, files[i], sizeof(FileInfoPacket))) != 0) {
+		// sizeof(FileInfoPacket)만큼 읽어들이지 못했다면 
+		while (recv_cnt < sizeof(FileInfoPacket)) {
+			buffer = read(sock, temp, sizeof(FileInfoPacket) - recv_cnt);
+			// printf("buffer[%dbyte]: %s\n", buffer, temp);
+			recv_cnt += buffer;
+		}
+
+		if (files[i]->size < 0) {
+			printf("%s\n", files[i]->name);
+			break;
+		}
+
+		i++;
+	}
+
+	if(recv_cnt == -1)
+	error_handling("read() error!");
+
+	return i;
+}
+
+/* read file list from server */
+void print_file_list(FileInfoPacket **files, int file_cnt)
+{
+	printf("\n---- file list ----\n");
+	for (int i = 0; i < file_cnt; i++) {
+		printf("[%s] %d. '%s', (%d Byte)\n", files[i]->type, i, files[i]->name, files[i]->size);
+	}
+	printf("\n");
+}
+
+void print_directories(FileInfoPacket **files, int file_cnt)
+{
+	printf("\n---- directory list ----\n");
+	for (int i = 0; i < file_cnt; i++) {
+		if (strcmp(files[i]->type, "dir") == 0) {
+			printf("[%s] %d. '%s', (%d Byte)\n", files[i]->type, i, files[i]->name, files[i]->size);
+		}
+	}
+	printf("\n");
+}
+
+void print_files(FileInfoPacket **files, int file_cnt)
+{
+	printf("\n---- file list ----\n");
+	for (int i = 0; i < file_cnt; i++) {
+		if (strcmp(files[i]->type, "file") == 0) {
+			printf("[%s] %d. '%s', (%d Byte)\n", files[i]->type, i, files[i]->name, files[i]->size);
+		}
+	}
+	printf("\n");
+}
+
+/* read file content from server and save file */
+int read_and_save_file(int sock, char* filename)
+{
+	char line[BUF_SIZE];
+	int recv_cnt, recv_len = 0;
+	FILE * fp;
+
+	// file open and read
+	if ((fp = fopen(filename, "wb")) == NULL) {
+		printf("Failed to open file.\n");
+		return 0;
+	} else {
+		while ((recv_cnt = read(sock, line, BUF_SIZE)) != 0) {
+			// printf("%s\n", line);
+			
+			fwrite(line, sizeof(char), recv_cnt, fp);
+		}
+	}
+	fclose(fp);
+
+	return 1;
+}
+
+void error_handling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+
+}
---
+++ hw3/client/file_info.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+// #include <error.h>
+#include <string.h>
+#include "file_info.h"
+
+/* return file size */
+int filesize(const char *filename)
+{
+    struct stat file_info;
+    int sz_file;
+
+    if (0 > stat(filename, &file_info)){
+	    return -1; // file이 없거나 에러
+    }
+    return file_info.st_size;
+}
+
+/* search file in directory */
+FileInfoPacket* search_file_in_directory()
+{
+    DIR *dir;
+    FileInfoPacket* files;
+    FileInfoPacket file;
+    struct dirent* directory;
+    int dirSize, i = 0;
+    void * tmp;
+    dir = opendir ("./");
+
+    directory = (struct dirent*)malloc((int)sizeof(struct dirent) * 100);
+    files = (FileInfoPacket*)malloc((int)sizeof(FileInfoPacket) * 100);
+
+    if (dir != NULL) {
+        /* print all the files and directories within directory */
+        while ((directory = readdir(dir)) != NULL) {
+            printf ("%s\n", directory->d_name);
+            strcpy(files[i].name, directory->d_name);
+            files[i].size = filesize(directory->d_name);
+
+            i++;
+        }
+        closedir (dir);
+    } else {
+         /* could not open directory */
+         perror ("Error: could not open directory");
+         exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
