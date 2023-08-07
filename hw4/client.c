79275bee5a5c5d399ab1a8d672115a7e961623a2
doplinBlue <doplinblue@handong.ac.kr>
2023-08-07 20:00:07 +0900

    hw4 echo server complete

--- hw4/client.c
+++ hw4/client.c
@@ -10,47 +10,132 @@
        - 검색어가 중간에 나오거나 끝에 나오는 경우 고려 (kmp algorithim)
     4. 검색어에 해당되는 부분은 임의의 색깔
 */
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <pthread.h>
 #include "Console.h"
 
 #define BUF_SIZE 1024
 
-int main()
+void * send_msg(void * arg);
+void * recv_msg(void * arg);
+void error_handling(char * msg);
+
+int len = 0;
+char search_word[BUF_SIZE] = {};
+char msg[BUF_SIZE] = {};
+
+int main(int argc, char *argv[])
 {
+    int sock;
+	struct sockaddr_in serv_addr;
+	pthread_t snd_thread, rcv_thread;
+	void * thread_return;
+
     char alpha;
-    char search_word[BUF_SIZE] = {};
     int len = 0;
+    
+	if(argc!=4) {
+		printf("Usage : %s <IP> <port>\n", argv[0]);
+		exit(1);
+	 }
+	
+	sock=socket(PF_INET, SOCK_STREAM, 0);
+	
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family=AF_INET;
+	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
+	serv_addr.sin_port=htons(atoi(argv[2]));
+	  
+	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
+		error_handling("connect() error");
 
+    // thread 열기
+    pthread_create(&snd_thread, NULL, send_msg, (void*)&sock);
+    pthread_create(&rcv_thread, NULL, recv_msg, (void*)&sock);
+    
+    // Client console 출력
     clrscr();
-
     while (1) {
-        EnableCursor(0);
+        clrscr();
+        EnableCursor(1);
         DrawBorderLine('-', '|', 5);
         gotoxy(2, 4);
-        printf("연관 검색어 List");
+        printf("* 연관 검색어 List");
+
+        gotoxy(3, 6);
+        printf("search_word: %s", search_word);
+
+        gotoxy(3, 8);
+        printf("msg: %s", msg);
         
-        EnableCursor(1);
         gotoxy(1, 1);
         printf("\033[38;2;159;75;153mSearch Word: ");
 
-        // 사용자에게 char 입력
         gotoxy(14, 1);
         printf("\033[38;2;255;255;255m%s", search_word);
-        alpha = getch();
-        clrscr();
+        EnableCursor(0);
+
+        // TODO: sever에게 검색어 보내기
+        MySleep(500);
+    }
+
+    pthread_join(snd_thread, &thread_return);
+    pthread_join(rcv_thread, &thread_return);
+    close(sock);
+
+    return 0;
+}
+
+void * send_msg(void * arg)   // send thread main
+{
+    char alpha;
+
+	int sock = *((int*)arg);
+	while(1) {
+        gotoxy(14+len, 1);
+		alpha = getch();
         printf("%c", alpha);
         
-        if (alpha == 127 || alpha == 8) {
+        if (alpha == 127 || alpha == 8) {       // backspace
             len--;
         } else { 
             search_word[len++] = alpha;
         }
         search_word[len] = 0;
-        gotoxy(3, 6);
-        printf("word: %s", search_word);
 
-        // TODO: sever에게 검색어 보내기
-    }
+		if (!strcmp(search_word,"q\n")||!strcmp(search_word,"Q\n")) {
+			close(sock);
+			exit(0);
+		}
 
-    return 0;
+		write(sock, search_word, strlen(search_word));
+	}
+	return NULL;
+}
+	
+void * recv_msg(void * arg)   // read thread main
+{
+	int sock = *((int*)arg);
+	int str_len;
+	while (1) {
+		str_len = read(sock, msg, BUF_SIZE-1);
+		if (str_len == -1) 
+			return (void*) -1;
+		msg[str_len] = 0;
+	}
+
+
+	return NULL;
+}
+	
+void error_handling(char *msg)
+{
+    // gotoxy(7, 6);
+	// printf("%s\n", msg);
+	exit(1);
 }
--- hw4/server.c
+++ hw4/server.c
@@ -11,11 +11,112 @@
     4. 검색어에 해당되는 부분은 임의의 색깔
 */
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
+
 #include "Console.h"
 
-int main()
+void * handle_clnt(void * arg);
+void send_msg(char * msg, int len, int clnt_sock);
+void error_handling(char * msg);
+
+#define BUF_SIZE 1024
+#define MAX_CLNT 256
+
+int clnt_cnt = 0;
+int clnt_socks[MAX_CLNT];
+pthread_mutex_t mutx;
+
+int main(int argc, char *argv[])
 {
-    // DrawBorderline('-', '|');
+    // Multi-Thread로 서버 구현 (TCP nodelay socket option 주기)
+    int serv_sock, clnt_sock;
+	struct sockaddr_in serv_adr, clnt_adr;
+	int clnt_adr_sz;
+	pthread_t t_id;
+	if(argc != 2) {
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+  
+	pthread_mutex_init(&mutx, NULL);
+	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
+
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family=AF_INET; 
+	serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
+	serv_adr.sin_port=htons(atoi(argv[1]));
+	
+	if(bind(serv_sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr))==-1)
+		error_handling("bind() error");
+	if(listen(serv_sock, 5)==-1)
+		error_handling("listen() error");
+	
+	while(1)
+	{
+		clnt_adr_sz = sizeof(clnt_adr);
+		clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr,&clnt_adr_sz);
+		
+		pthread_mutex_lock(&mutx);
+		clnt_socks[clnt_cnt++] = clnt_sock;
+		pthread_mutex_unlock(&mutx);
+	
+		pthread_create(&t_id, NULL, handle_clnt, (void*)&clnt_sock);
+		pthread_detach(t_id);
+		printf("Connected client IP: %s \n", inet_ntoa(clnt_adr.sin_addr));
+	}
+	close(serv_sock);
+
+    // ./search_server 9090 data.txt
+    // 파일 읽어와 Trie에 저장
+
+    // 검색어 중간/끝에 나오는 경우 고려
 
     return 0;
+}
+
+void * handle_clnt(void * arg)
+{
+	int clnt_sock = *((int*)arg);
+	int str_len=0, i;
+	char msg[BUF_SIZE] = {};
+	
+	while ((str_len = read(clnt_sock, msg, sizeof(msg))) != 0) {
+		msg[str_len] = 0;
+        send_msg(msg, str_len, clnt_sock);
+        printf("msg: %s\n", msg);
+    }
+	
+	// remove disconnected client
+	pthread_mutex_lock(&mutx);
+	for (i = 0; i < clnt_cnt; i++) {
+		if (clnt_sock == clnt_socks[i]) {
+			while(i++ < clnt_cnt-1)
+				clnt_socks[i] = clnt_socks[i+1];
+			break;
+		}
+	}
+	clnt_cnt--;
+	pthread_mutex_unlock(&mutx);
+	close(clnt_sock);
+	return NULL;
+}
+
+void send_msg(char * msg, int len, int clnt_sock)   // send to all
+{
+	int send_len;
+	send_len = write(clnt_sock, msg, len);
+	printf("send_len: %d\n", send_len);
+}
+
+void error_handling(char * msg)
+{
+	fputs(msg, stderr);
+	fputc('\n', stderr);
+	exit(1);
 }
